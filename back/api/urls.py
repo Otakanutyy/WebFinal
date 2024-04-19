d5bcc45fbb576b8f500641816dcec4bbd874f422
Islam <kogsaitama@gmail.com>
2024-04-19 19:30:46 +0500

    bugfix

--- back/api/urls.py
+++ back/api/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('/login/', TokenObtainPairView.as_view()),
-    path('/refresh/', TokenRefreshView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
 ]
